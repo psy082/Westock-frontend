1b4f4863d16fa5a0557e3797cdc8c05519ea3149
psy082 <zhappy114@gmail.com>
2020-09-02 22:26:09 +0900

    Modify: 초기 세팅 수정 - Routes uri 변경 및 추가 - pull request template 추가

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import MyAccount from "./Pages/Account/MyAccount/MyAccount";
+import Login from "./Pages/Account/Login/Login";
 import ItemDetail from "./Pages/ItemDetail/ItemDetail";
 import ItemList from "./Pages/ItemList/ItemList";
 
@@ -12,8 +13,9 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/account" component={MyAccount} />
+          <Route exact path="/account/login" component={Login} />
           <Route exact path="/:itemName" component={ItemDetail} />
-          <Route exact path="/air-jordans/:itemName" component={ItemList} />
+          <Route exact path="/sneakers/" component={ItemList} />
         </Switch>
       </Router>
     );
